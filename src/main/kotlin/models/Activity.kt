7545a39f38d5092fa5157e901855c8bf16f8e85c
tobsirl <tobster111@gmail.com>
2017-12-26 19:46:26 +0000

    Initial Commit

---
+++ src/main/kotlin/models/Activity.kt
@@ -0,0 +1,11 @@
+package models
+
+import java.util.UUID
+import kotlin.collections.ArrayList
+
+data class Activity(
+        var type: String = "",
+        var location: String = "",
+        var distance: Float = 0.0f,
+        var id: String = UUID.randomUUID().toString(),
+        val route: MutableList<Location> = ArrayList())
